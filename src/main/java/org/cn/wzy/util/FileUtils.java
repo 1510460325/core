06d53f85c11ac3e9973d28b99e87619fcba9f996
1510460325@qq.com <1510460325@qq.com>
2018-11-08 08:36:21 +0800

    添加zip的压缩解压缩工具类

--- src/main/java/org/cn/wzy/util/FileUtils.java
+++ src/main/java/org/cn/wzy/util/FileUtils.java
@@ -19,102 +19,102 @@ import java.util.List;
  * on 2018/6/14 15:41
  * 不短不长八字刚好
  */
-public class FileUtils extends org.apache.commons.io.FileUtils{
+public class FileUtils extends org.apache.commons.io.FileUtils {
 
-    public static final void write(InputStream inputStream, OutputStream outputStream, boolean close) throws IOException {
-        byte[] by = new byte[1024];
-        int n;
-        while ((n = inputStream.read(by)) != -1) {
-            outputStream.write(by, 0, n);
-        }
-        if (close) {
-            inputStream.close();
-            outputStream.close();
-        }
+  public static void write(InputStream inputStream, OutputStream outputStream, boolean close) throws IOException {
+    byte[] by = new byte[1024];
+    int n;
+    while ((n = inputStream.read(by)) != -1) {
+      outputStream.write(by, 0, n);
     }
-
-    public static final List<String> read(InputStream inputStream,String charsetName) {
-        InputStreamReader isr = null;
-        try {
-            isr = new InputStreamReader(inputStream,charsetName);
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-            return null;
-        }
-        BufferedReader reader = new BufferedReader(isr);
-        String str;
-        List<String> rs = new ArrayList<>();
-        try {
-            while ((str = reader.readLine()) != null) {
-                rs.add(str);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        } finally {
-            try {
-                reader.close();
-                isr.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return rs;
+    if (close) {
+      inputStream.close();
+      outputStream.close();
     }
+  }
 
-    public static final List<FileItem> getFileList(HttpServletRequest request, String tempPath) throws FileUploadException {
-        File tmpFile = new File(tempPath);
-        if (!tmpFile.exists()) {
-            tmpFile.mkdir();
-        }
-        DiskFileItemFactory factory = new DiskFileItemFactory();
-        factory.setSizeThreshold(1024 * 100);
-        factory.setRepository(tmpFile);
-        ServletFileUpload upload = new ServletFileUpload(factory);
-        upload.setHeaderEncoding("UTF-8");
-        upload.setFileSizeMax(PropertiesUtil.LongValue("fileSizeMax"));
-        upload.setSizeMax(PropertiesUtil.LongValue("fileTotalSizeMax"));
-        return upload.parseRequest(request);
+  public static List<String> read(InputStream inputStream, String charsetName) {
+    InputStreamReader isr = null;
+    try {
+      isr = new InputStreamReader(inputStream, charsetName);
+    } catch (UnsupportedEncodingException e) {
+      e.printStackTrace();
+      return null;
     }
-
-    public static final boolean download(String path, String fileName, String file) {
-        File parent = new File(path);
-        if (!parent.exists()) {
-            parent.mkdirs();
-        }
-        File target = new File(parent + "/" + fileName);
-        if (!target.exists()) {
-            try {
-                target.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-                return false;
-            }
-        }
-        BASE64Decoder decoder = new BASE64Decoder();
-        try {
-            byte[] b = decoder.decodeBuffer(file);
-            for (int i = 0; i < b.length; ++i) {
-                if (b[i] < 0) {// 调整异常数据
-                    b[i] += 256;
-                }
-            }
-            OutputStream out = new FileOutputStream(target);
-            out.write(b);
-            out.flush();
-            out.close();
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
+    BufferedReader reader = new BufferedReader(isr);
+    String str;
+    List<String> rs = new ArrayList<>();
+    try {
+      while ((str = reader.readLine()) != null) {
+        rs.add(str);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+      return null;
+    } finally {
+      try {
+        reader.close();
+        isr.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
+    return rs;
+  }
 
-
-    public static final void download(String fileName, InputStream inputStream, HttpServletResponse response) throws IOException {
-        response.setContentType("application/x-msdownload");
-        response.setHeader("Content-Disposition", "attachment;filename=" + "\"" + new Date().getTime() + fileName + "\"");
-        ServletOutputStream outputStream = response.getOutputStream();
-        write(inputStream, outputStream, true);
+  public static List<FileItem> getFileList(HttpServletRequest request, String tempPath) throws FileUploadException {
+    File tmpFile = new File(tempPath);
+    if (!tmpFile.exists()) {
+      tmpFile.mkdir();
     }
+    DiskFileItemFactory factory = new DiskFileItemFactory();
+    factory.setSizeThreshold(1024 * 100);
+    factory.setRepository(tmpFile);
+    ServletFileUpload upload = new ServletFileUpload(factory);
+    upload.setHeaderEncoding("UTF-8");
+    upload.setFileSizeMax(PropertiesUtil.LongValue("fileSizeMax"));
+    upload.setSizeMax(PropertiesUtil.LongValue("fileTotalSizeMax"));
+    return upload.parseRequest(request);
+  }
+
+  public static boolean download(String path, String fileName, String file) {
+    File parent = new File(path);
+    if (!parent.exists()) {
+      parent.mkdirs();
+    }
+    File target = new File(parent + "/" + fileName);
+    if (!target.exists()) {
+      try {
+        target.createNewFile();
+      } catch (IOException e) {
+        e.printStackTrace();
+        return false;
+      }
+    }
+    BASE64Decoder decoder = new BASE64Decoder();
+    try {
+      byte[] b = decoder.decodeBuffer(file);
+      for (int i = 0; i < b.length; ++i) {
+        if (b[i] < 0) {// 调整异常数据
+          b[i] += 256;
+        }
+      }
+      OutputStream out = new FileOutputStream(target);
+      out.write(b);
+      out.flush();
+      out.close();
+      return true;
+    } catch (Exception e) {
+      e.printStackTrace();
+      return false;
+    }
+  }
+
+
+  public static void download(String fileName, InputStream inputStream, HttpServletResponse response) throws IOException {
+    response.setContentType("application/x-msdownload");
+    response.setHeader("Content-Disposition", "attachment;filename=" + "\"" + new Date().getTime() + fileName + "\"");
+    ServletOutputStream outputStream = response.getOutputStream();
+    write(inputStream, outputStream, true);
+  }
 }
